56e9c544985fba11b38e0819722aced4fe121c94
Carlos Alberto <merecmoney3@gmail.com>
2020-10-11 19:12:44 -0500

    initial commit

---
+++ typeScriptEslint.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+npm init -y
+npm install eslint --save-dev
+npm install typescript --save-dev
+npm install eslint-config-prettier --save-dev
+npm install eslint-plugin-prettier --save-dev
