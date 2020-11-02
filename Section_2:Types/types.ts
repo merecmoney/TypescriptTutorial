ec8982782261c29573b79fc5fc879880c5aaebb3
Carlos Alberto <merecmoney3@gmail.com>
2020-11-01 19:41:08 -0600

    Add types directory

---
+++ Section_2:Types/types.ts
@@ -0,0 +1,11 @@
+const today = new Date();
+today.getMonth();
+
+const person = {
+    age: 30
+}; //no defined type, just assures
+// all properties and method defined for the object can be used
+
+class Color {}
+
+const red = new Color();
---
+++ Section_2:Types/variables.ts
@@ -0,0 +1,29 @@
+//Type annotation
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//built-in object
+
+let now: Date = new Date();
+
+// array
+let color: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true];
+
+// Classes
+class Car {
+}
+
+let car: Car = new Car;
+
+// Object Literal
+
+let point: { x: number; y: number } = {
+    x: 10,
+    y: 20
+};
